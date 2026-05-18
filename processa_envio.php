545d0b97b05de8a75001a494150898bb46dbf061
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-18 15:32:50 -0300

    update

--- processa_envio.php
+++ processa_envio.php
@@ -39,6 +39,7 @@
         if(!$mensagem->mensagemValida()){
             echo 'Mensagem não é válida';
             die();
+            header('Location: index.php');
         } 
     }
 
@@ -46,35 +47,35 @@
     $mail = new PHPMailer(true);
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->SMTPDebug = 0;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'lbm3@discente.ifpe.edu.br';                     //SMTP username
         $mail->Password   = 'vdnv csqf vbyr fluf';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients -- metodos do objeto
         $mail->setFrom('lbm3@discente.ifpe.edu.br', 'Estudante Leonardo');
-        $mail->addAddress('lbm3@discente.ifpe.edu.br', 'Estudante Leonardo');     //Add a recipient
+        $mail->addAddress($mensagem-> __get('para'));     //Add a recipient
         $mail->addAddress('ellen@example.com');               //Name is optional
         $mail->addReplyTo('info@example.com', 'Information');
         //$mail->addCC('cc@example.com'); --> copia
         //$mail->addBCC('bcc@example.com'); --> copia oculta
 
         //Attachments
-        $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Oi, eu sou o assunto!';
-        $mail->Body    = 'Oi, eu sou o conteúdo do <strong>e-mail</strong>';
-        $mail->AltBody = 'Oi, eu sou o conteúdo do e-mail';
+        $mail->Subject = $mensagem-> __get('assunto');
+        $mail->Body    = $mensagem->__get('mensagem');
+        $mail->AltBody = 'É necessario utilizar o client que suporte o HTML';
 
         $mail->send();
-        echo 'Message has been sent';
+        echo 'E-mail enviado com sucesso';
     } catch (Exception $e) {
         echo "Não foi possível enviar esse e-mail! Tente novamente mais tarde";
         echo "Detalhes do erro: {$mail->ErrorInfo}";
