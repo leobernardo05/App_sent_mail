e496c37beb814d9e2757c4a1aac0b38db59f2c30
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-18 12:07:41 -0300

    update

--- processa_envio.php
+++ processa_envio.php
@@ -1,64 +1,67 @@
 <?php
-require "./bibliotecas/src/Exception.php";
-require "./bibliotecas/src/OAuth.php";
-require "./bibliotecas/src/PHPMAiler.php";
-require "./bibliotecas/src/POP3.php";
-require "./bibliotecas/src/SMTP.php";
+    require "./bibliotecas/src/Exception.php";
+    require "./bibliotecas/src/OAuth.php";
+    require "./bibliotecas/src/PHPMailer.php";
+    require "./bibliotecas/src/POP3.php";
+    require "./bibliotecas/src/SMTP.php";
 
-use PHOMailer\PHPMailer\PHPMailer;
-use PHOMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
-class Mensagem {
-    private $para = null;
-    private $assunto = null;
-    private $mensagem = null;
+    class Mensagem {
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
 
-    public function __get($atributo){
-        return $this->$atributo;
+        public function __get($atributo){
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor){
+            $this->$atributo = $valor;
+        }
+
+        public function mensagemValida(){
+            return !empty($this->para) &&
+                !empty($this->assunto) &&
+                !empty($this->mensagem);
+        }
     }
 
-    public function __set($atributo, $valor){
-        $this->$atributo = $valor;
+    if($_SERVER["REQUEST_METHOD"] === "POST") {
+
+        $mensagem = new Mensagem();
+
+        $mensagem->__set('para', $_POST['para'] ?? '');
+        $mensagem->__set('assunto', $_POST['assunto'] ?? '');
+        $mensagem->__set('mensagem', $_POST['mensagem'] ?? '');
+
+        if(!$mensagem->mensagemValida()){
+            echo 'Mensagem não é válida';
+            die();
+        } 
     }
 
-    public function mensagemValida(){
-        return !empty($this->para) &&
-               !empty($this->assunto) &&
-               !empty($this->mensagem);
-    }
-}
-
-if($_SERVER["REQUEST_METHOD"] === "POST") {
-
-    $mensagem = new Mensagem();
-
-    $mensagem->__set('para', $_POST['para'] ?? '');
-    $mensagem->__set('assunto', $_POST['assunto'] ?? '');
-    $mensagem->__set('mensagem', $_POST['mensagem'] ?? '');
-
-    if(!$mensagem->mensagemValida()){
-        echo 'Mensagem não é válida';
-    } 
-
+    // implementação do PHPMailer, para poder fazer requisições HTTP
     $mail = new PHPMailer(true);
     try {
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'user@example.com';                     //SMTP username
-        $mail->Password   = 'secret';                               //SMTP password
+        $mail->Username   = 'lbm3@discente.ifpe.edu.br';                     //SMTP username
+        $mail->Password   = 'SENHA_DE_APP_AQUI';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-        //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+        //Recipients -- metodos do objeto
+        $mail->setFrom('lbm3@discente.ifpe.edu.br', 'Estudante Leonardo');
+        $mail->addAddress('lbm3@discente.ifpe.edu.br', 'Estudante Leonardo');     //Add a recipient
         $mail->addAddress('ellen@example.com');               //Name is optional
         $mail->addReplyTo('info@example.com', 'Information');
-        $mail->addCC('cc@example.com');
-        $mail->addBCC('bcc@example.com');
+        //$mail->addCC('cc@example.com'); --> copia
+        //$mail->addBCC('bcc@example.com'); --> copia oculta
 
         //Attachments
         $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
@@ -66,14 +69,15 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
 
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail->Subject = 'Oi, eu sou o assunto!';
+        $mail->Body    = 'Oi, eu sou o conteúdo do <strong>e-mail</strong>';
+        $mail->AltBody = 'Oi, eu sou o conteúdo do e-mail';
 
         $mail->send();
         echo 'Message has been sent';
     } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        echo "Não foi possível enviar esse e-mail! Tente novamente mais tarde";
+        echo "Detalhes do erro: {$mail->ErrorInfo}";
     }
 
 ?>
